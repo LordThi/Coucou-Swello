cc31a561322b2a89862205653bc24a4fd4e68095
LordThi <92553090+LordThi@users.noreply.github.com>
2023-02-24 19:52:31 +0100

    :sparkles: feat(toggler): new feature-> disparition of connexion button when LoginForm is open

--- src/components/Toggler/index.js
+++ src/components/Toggler/index.js
@@ -4,10 +4,19 @@ const Toggler = ({
   onEmitClick,
   isOpen
 }) => {
-    const classNameCSS = isOpen ? "toggler toggler--open" : "toggler"
-    return (
-      <button className={classNameCSS} type="button" onClick={onEmitClick}>x</button>
-    );
+  const classNameCSS = isOpen ? "toggler toggler--open" : "toggler"
+  console.log(isOpen)
+  return (
+
+    !isOpen &&
+      (<button 
+          className={classNameCSS}
+          type="button"
+          onClick={onEmitClick}>
+            Connexion
+      </button>)
+
+  );
 };
 
 export default Toggler;
