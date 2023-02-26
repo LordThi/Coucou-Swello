1ddffef4be841a0097e9d7039d339057f89f7cda
LordThi <92553090+LordThi@users.noreply.github.com>
2023-02-26 16:32:47 +0100

    :sparkles: (Main): add Main compnt and styles

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,14 @@
 // == Import
 import './styles.scss';
 import Header from '../Header';
+import Main from '../Main';
 
 // == Composant
 function App() {
   return (
     <div className="app">
-      
       <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main/index.js
@@ -0,0 +1,15 @@
+import './styles.scss';
+
+const Main = () => {
+  return (
+    <div className='main__container'>
+      <div className='left-background-triangle'></div>
+      <div className='message__container'>
+        <p>test blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclaest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclaest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclaest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclaest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclaest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclaest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclaest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablaclatest blablacla</p>
+      </div>
+      <div className='right-background-triangle'></div>
+    </div>
+  )
+};
+
+export default Main;
