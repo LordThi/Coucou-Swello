917e96d929de1a692fac365ba16e40e42eb2d916
LordThi <92553090+LordThi@users.noreply.github.com>
2023-02-28 00:34:35 +0100

    :bug: fix(Carrousel): fix use of swiper, css sliders, autoplay,loop, pagination

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,47 +1,53 @@
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination, Navigation } from "swiper";
-
-import './styles.scss';
 import "swiper/css";
+import './styles.scss';
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
 import recommendations from '../../data/MOCK_ recommendations.json';
+import fivestar from '../../assets/img/Five-Star.png';
+
+
+import { Autoplay, Pagination, Navigation } from "swiper";
 
 const Carousel = () => {
 
   return (
 
     <div className='carousel__container'>
-
       <Swiper
-        direction={"vertical"}
-        slidesPerView={3}
-        spaceBetween={1}
+        slidesPerView={2}
+        spaceBetween={30}
+        centeredSlides={false}
         loop={true}
         autoplay={{
-          delay: 1000,
+          delay: 2500,
           disableOnInteraction: false,
         }}
         pagination={{
           clickable: true,
         }}
-        navigation={false}
-        modules={[Autoplay, Pagination, Navigation]}
+        navigation={true}
+        modules={[Autoplay, Pagination]}
         className="mySwiper"
       >
         {
           recommendations.map((recommendation) => (
+          <SwiperSlide key={recommendation.id} className={`social-media--card ${recommendation.category}`}>
 
-            <SwiperSlide key={recommendation.id} className={`social-media--card ${recommendation.category}`}>
-              <div className={recommendation.img}></div>
-              <p className='message'>{recommendation.msg}</p>
-              <p className=''>{recommendation.author}</p>
+            <div className="card__img">
+              <img src={recommendation.img} alt=""/>
+            </div>
+            <div className="card__comment">
+              <p className="card__message">{recommendation.msg}</p>
+              <img  className="card__stars" src={fivestar} alt="" />
+              <p className="card__author">{recommendation.author}</p>
+              <p className="card__job">{recommendation.job}</p>
+            </div>
           </SwiperSlide>
           ))
         }
       </Swiper>
-
     </div>
-
   );
 };
 
