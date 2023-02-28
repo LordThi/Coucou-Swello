c22f2b077af1bf889c0e8f82c52533d2c384571b
LordThi <92553090+LordThi@users.noreply.github.com>
2023-02-28 14:55:24 +0100

    :sparkles: feat (Main): add whoami section

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -6,21 +6,35 @@ import Carousel from '../Carousel';
 const Main = () => {
   return (
     <div className='main__container'>
-      <div className='left-background-triangle'></div>
-      <div className='main__inner'>
-        <div className='message__container'>
-          <h1 className='main__title'>Coucou Swello 👋</h1>
-          <h2 className='main__subtitle'>Moi c'est Thibaut, votre prochain stagiaire.</h2>
-          <p className='motivation__text'>Je sais que vous ne lisez pas les lettres de motivation alors j'ai dû ruser pour vous montrer la mienne. Je suis votre aventure sur les réseaux sociaux depuis le début de ma reconversion profesionnelle et plus le temps passe, plus j'ai envie de monter dans le bateau et faire partie de votre aventure.</p>
+      <div className='main__intro'>
+        <div className='left-background-triangle'></div>
+        <div className='main__inner'>
+          <div className='message__container'>
+            <h1 className='main__title'>Coucou Swello 👋</h1>
+            <h2 className='main__subtitle'>Moi c'est Thibaut, votre prochain stagiaire.</h2>
+            <p className='motivation__text'>
+              Je sais que vous ne lisez pas les lettres de motivation alors j'ai dû ruser pour vous montrer la mienne. Je suis votre aventure sur les réseaux sociaux depuis le début de ma reconversion profesionnelle et plus le temps passe, plus j'ai envie de monter dans le bateau et faire partie de votre aventure
+            </p>
+          </div>
+          <div className='right__container'>
+            <img className='img__carousel' src={deadCarousel} alt="" />
+          </div>
+          <img className='man__chilling' src={manChiling}></img>
         </div>
-        <div className='right__container'>
-          <img className='img__carousel' src={deadCarousel} alt="" />
+        <div className='right__triangle-wraper'>
+          <div className='right-background-triangle'></div>
         </div>
-        <img className='man__chilling' src={manChiling}></img>
       </div>
-      <div className='right__triangle-wraper'>
-        <div className='right-background-triangle'></div>
-      </div>                                                                      
+      <div className='main__whoami'>
+        <h1 className='whoami__title'>Ce que vous n'apprendrez pas dans mon CV</h1>
+        <ul>
+          <li>Date de création de mon compte facebook : 12 nov. 2007</li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
     </div>
   )
 };
