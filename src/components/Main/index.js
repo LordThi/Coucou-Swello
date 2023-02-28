6b7976243fe9421c152ff50d7bd146e94b3a15bc
LordThi <92553090+LordThi@users.noreply.github.com>
2023-02-28 14:10:10 +0100

    :bug: fix(main): fix """responsive"""

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -7,8 +7,8 @@ const Main = () => {
   return (
     <div className='main__container'>
       <div className='left-background-triangle'></div>
-      {/* <div className='main__inner'> */}
-        {/* <div className='message__container'>
+      <div className='main__inner'>
+        <div className='message__container'>
           <h1 className='main__title'>Coucou Swello 👋</h1>
           <h2 className='main__subtitle'>Moi c'est Thibaut, votre prochain stagiaire.</h2>
           <p className='motivation__text'>Je sais que vous ne lisez pas les lettres de motivation alors j'ai dû ruser pour vous montrer la mienne. Je suis votre aventure sur les réseaux sociaux depuis le début de ma reconversion profesionnelle et plus le temps passe, plus j'ai envie de monter dans le bateau et faire partie de votre aventure.</p>
@@ -16,8 +16,8 @@ const Main = () => {
         <div className='right__container'>
           <img className='img__carousel' src={deadCarousel} alt="" />
         </div>
-        <img className='man__chilling' src={manChiling}></img> */}
-      {/* </div> */}
+        <img className='man__chilling' src={manChiling}></img>
+      </div>
       <div className='right__triangle-wraper'>
         <div className='right-background-triangle'></div>
       </div>                                                                      
