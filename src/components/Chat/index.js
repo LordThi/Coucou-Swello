2f0d4fb452a45482aff65b9445ee1167cbfd17e5
LordThi <92553090+LordThi@users.noreply.github.com>
2023-03-02 04:03:37 +0100

    :sparkles: feat(Chat): introducing Chat component

---
+++ src/components/Chat/index.js
@@ -0,0 +1,16 @@
+import './styles.scss';
+
+const Chat = () => {
+  return (
+    <div className="chat">
+      <div className='chat__container'>
+
+      </div>
+      <div className='chat__icon'>
+
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
